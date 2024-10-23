1a5e5875ca3891c3e13298ffd82336f77ee169a3
fluxdev13smart <165156810+fluxdev13smart@users.noreply.github.com>
2024-10-23 20:19:53 +0400

    Update break_bot.py

--- break_bot.py
+++ break_bot.py
@@ -5,7 +5,7 @@ import os
 
 # Load environment variables
 load_dotenv()
-TOKEN = os.getenv('MTI5ODY3OTk2MDI4NjAwNzM0OQ.GbdfET.lunoM8tG0uMKSr6WWPJtWfyfPPg7BvskHbQKq8')
+TOKEN = os.getenv('MTI5ODY3OTk2MDI4NjAwNzM0OQ.GgBmEi.mQ95_n_h-l6C3QmdS_Wl-TisIzuCxMdP1g4aLg')
 
 # Set intents
 intents = discord.Intents.default()
